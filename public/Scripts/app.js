e89b305ac0b0ff3f4dfdd5a3276909fae108bf46
parisa <parisa.modirshahla@gmail.com>
2021-09-16 09:54:05 -0400

    Added index / css / js files

---
+++ public/Scripts/app.js
@@ -0,0 +1,15 @@
+
+/*js goes here */
+
+// IIFE - Immediately Invoked Function Expression (AKA an anonymous self-executing function)
+
+(function(){
+
+    function Start()
+    {
+        console.log("App Started...");
+    }
+
+    window.addEventListener("load", Start);
+
+})();
